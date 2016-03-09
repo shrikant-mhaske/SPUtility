a56b9d1f8664d279d1130081b3110f9a46b3550d
Shrikant Vasant Mhaske <shrikant.mhaske@sikkasoftware.com>
2016-03-09 13:36:28 +0530

    Add

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+namespace RawData
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            RawDataBusinessLogic objRawDataBusinessLogic = new RawDataBusinessLogic();
+            //Comment
+            //get cust list
+            DataTable custdt = objRawDataBusinessLogic.getCusomerlist();
+            //get query list
+            DataTable querydt = objRawDataBusinessLogic.getQuerylist();
+            //get data one by one masterID
+            objRawDataBusinessLogic.getDataOneByOne(custdt, querydt);
+        }
+    }
+}
